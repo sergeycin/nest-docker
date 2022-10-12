035b02fbdeae712e4e64d86aa8509eaef33a63f8
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-10-12 22:18:56 +0300

    controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { createUserDto } from './dto/create-user.dto';
+
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private userService:UsersService){}
+
+    @Post()
+    create(@Body() userDto: createUserDto){
+        return this.userService.createUser(userDto)
+    }
+
+    @Get()
+    getAll(){
+        return this.userService.getAllUsers()
+    }
+    
+}
